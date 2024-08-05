afe00efeb61dc99a4b007716cf7ced999f917207
alpaparmar <alpaparmar1720@gmail.com>
2024-08-05 12:59:48 +0530

    initial commit

---
+++ next.config.js
@@ -0,0 +1,26 @@
+const sourcebit = require('sourcebit');
+const sourcebitConfig = require('./sourcebit.js');
+
+sourcebit.fetch(sourcebitConfig);
+
+module.exports = {
+    trailingSlash: false,
+    eslint: {
+        // Allow production builds to successfully complete even if your project has ESLint errors.
+        ignoreDuringBuilds: true
+    },
+    webpack: (config, { dev }) => {
+        // Tell webpack to ignore watching content files in the content folder.
+        // Otherwise webpack recompiles the app and refreshes the whole page.
+        // Instead, the src/pages/[...slug].js uses the "withRemoteDataUpdates"
+        // function to update the content on the page without refreshing the
+        // whole page
+        config.watchOptions.ignored.push('/content/');
+        if (dev) {
+            // enable tree shaking for development mode, on production it is on by default
+            config.optimization.usedExports = false; // changed from true on 8/15/22 to resolve error when upgrading NextJS from v11.1.4 to v12.1.0
+        }
+
+        return config;
+    }
+}
